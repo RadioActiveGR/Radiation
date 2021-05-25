4a1e2161da25790fcf790645cf814db49d7b270a
Michael Fronistas <m.k.fronistas2@newcastle.ac.uk>
2021-05-25 20:56:03 +0100

    Initial Commit

---
+++ cli.py
@@ -0,0 +1,150 @@
+import time
+import response
+import os
+
+
+
+def main():
+    while (True):
+        clear()
+        print('''
+ /$$$$$$$                      /$$       /$$      /$$                 /$$ /$$  /$$$$$$  /$$                    
+| $$__  $$                    | $$      | $$$    /$$$                | $$|__/ /$$__  $$|__/                    
+| $$  \ $$  /$$$$$$  /$$$$$$$ | $$   /$$| $$$$  /$$$$  /$$$$$$   /$$$$$$$ /$$| $$  \__/ /$$  /$$$$$$   /$$$$$$ 
+| $$$$$$$/ |____  $$| $$__  $$| $$  /$$/| $$ $$/$$ $$ /$$__  $$ /$$__  $$| $$| $$$$    | $$ /$$__  $$ /$$__  $$
+| $$__  $$  /$$$$$$$| $$  \ $$| $$$$$$/ | $$  $$$| $$| $$  \ $$| $$  | $$| $$| $$_/    | $$| $$$$$$$$| $$  \__/
+| $$  \ $$ /$$__  $$| $$  | $$| $$_  $$ | $$\  $ | $$| $$  | $$| $$  | $$| $$| $$      | $$| $$_____/| $$      
+| $$  | $$|  $$$$$$$| $$  | $$| $$ \  $$| $$ \/  | $$|  $$$$$$/|  $$$$$$$| $$| $$      | $$|  $$$$$$$| $$      
+|__/  |__/ \_______/|__/  |__/|__/  \__/|__/     |__/ \______/  \_______/|__/|__/      |__/ \_______/|__/         \n
+== == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == \n
+By RadioActive\n
+== == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == \n
+Cookie: ''')
+
+        cookie = input()
+        cookie = cookie.rstrip("\n").rstrip("\n").rstrip("\n").rstrip("\n")
+        clear()
+
+
+        if response.checkCookie(cookie) == True:
+            print("Correct Cookie!")
+            selection = -1
+            while selection != 3:
+                clear()
+                print_menu()
+                try:
+                    selection = int(input())
+                except ValueError:
+                    print("Input an integer")
+
+                if selection == 1:
+                    c = -1
+                    while c != 3:
+                        clear()
+                        print(''' 
+  ______                                  __                               
+ /      \                                /  |                              
+/$$$$$$  | __    __   ______   __     __ $$/  __     __  ______    ______  
+$$ \__$$/ /  |  /  | /      \ /  \   /  |/  |/  \   /  |/      \  /      \ 
+$$      \ $$ |  $$ |/$$$$$$  |$$  \ /$$/ $$ |$$  \ /$$//$$$$$$  |/$$$$$$  |
+ $$$$$$  |$$ |  $$ |$$ |  $$/  $$  /$$/  $$ | $$  /$$/ $$ |  $$ |$$ |  $$/ 
+/  \__$$ |$$ \__$$ |$$ |        $$ $$/   $$ |  $$ $$/  $$ \__$$ |$$ |      
+$$    $$/ $$    $$/ $$ |         $$$/    $$ |   $$$/   $$    $$/ $$ |      
+ $$$$$$/   $$$$$$/  $$/           $/     $$/     $/     $$$$$$/  $$/      \n
+== == == == == == == == == == == == == == == == == == == == == == == == ==\n''' ,"\n"
+"Your Survivor Rank is ", response.rankGetSurv(cookie), "\n"
+"== == == == == == == == == == == == == == == == == == == == == == == == ==\n"
+"Enter rank: \n")
+                        try:
+                            c = int(input())
+                        except ValueError:
+                            print("Input an integer")
+                        if c < 1 or c > 20:
+                            print("Please enter a valid rank")
+                        else:
+                            if c < response.rankGetSurv(cookie):
+                                while c != response.rankGetSurv(cookie):
+                                    response.rankUpSurv(cookie)
+                            if c > response.rankGetSurv(cookie):
+                                while c != response.rankGetSurv(cookie):
+                                    response.rankDownSurv(cookie)
+                            if c == response.rankGetSurv(cookie):
+                                print("Your rank is ", c)
+                                break
+                    time.sleep(1)
+                if selection == 2:
+                    b = -1
+                    while True:
+                        clear()
+                        print('''
+ __    __  __  __  __                     
+|  \  /  \|  \|  \|  \                    
+| $$ /  $$ \$$| $$| $$  ______    ______  
+| $$/  $$ |  \| $$| $$ /      \  /      \ 
+| $$  $$  | $$| $$| $$|  $$$$$$\|  $$$$$$\|
+| $$$$$\  | $$| $$| $$| $$    $$| $$   \$$
+| $$ \$$\ | $$| $$| $$| $$$$$$$$| $$      
+| $$  \$$\| $$| $$| $$ \$$     \| $$      
+ \$$   \$$ \$$ \$$ \$$  \$$$$$$$ \$$         \n 
+== == == == == == == == == == == == == ==\n''',
+"Your Killer Rank is ", response.rankGetKiller(cookie),"\n"
+"== == == == == == == == == == == == == ==\n"
+"Enter rank: \n")
+                        try:
+                            b = int(input())
+                        except ValueError:
+                            print("Input an integer")
+                        if b < 1 or b > 20:
+                            print("Please enter a valid rank")
+                        else:
+                            if b < response.rankGetKiller(cookie):
+                                while b != response.rankGetKiller(cookie):
+                                    response.rankUpKiller(cookie)
+                            if b > response.rankGetKiller(cookie):
+                                while b != response.rankGetKiller(cookie):
+                                    response.rankDownKiller(cookie)
+                            if b == response.rankGetKiller(cookie):
+                                print("Your rank is ", b)
+                                break
+                    time.sleep(1)
+                if selection == 3:
+                    print("Exiting..")
+                    exit()
+                else:
+                    print("Please provide a valid value")
+                    time.sleep(2)
+        else:
+            print("Invalid Cookie")
+            time.sleep(1)
+
+
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+
+
+    else:
+        _ = os.system('clear')
+
+
+def get_cookie():
+    print("Cookie:")
+
+
+def print_menu():
+    print('''\n
+ /$$$$$$$                      /$$       /$$      /$$                 /$$ /$$  /$$$$$$  /$$                    
+| $$__  $$                    | $$      | $$$    /$$$                | $$|__/ /$$__  $$|__/                    
+| $$  \ $$  /$$$$$$  /$$$$$$$ | $$   /$$| $$$$  /$$$$  /$$$$$$   /$$$$$$$ /$$| $$  \__/ /$$  /$$$$$$   /$$$$$$ 
+| $$$$$$$/ |____  $$| $$__  $$| $$  /$$/| $$ $$/$$ $$ /$$__  $$ /$$__  $$| $$| $$$$    | $$ /$$__  $$ /$$__  $$
+| $$__  $$  /$$$$$$$| $$  \ $$| $$$$$$/ | $$  $$$| $$| $$  \ $$| $$  | $$| $$| $$_/    | $$| $$$$$$$$| $$  \__/
+| $$  \ $$ /$$__  $$| $$  | $$| $$_  $$ | $$\  $ | $$| $$  | $$| $$  | $$| $$| $$      | $$| $$_____/| $$      
+| $$  | $$|  $$$$$$$| $$  | $$| $$ \  $$| $$ \/  | $$|  $$$$$$/|  $$$$$$$| $$| $$      | $$|  $$$$$$$| $$      
+|__/  |__/ \_______/|__/  |__/|__/  \__/|__/     |__/ \______/  \_______/|__/|__/      |__/ \_______/|__/                                                                                                       
+== == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == \n
+by RadioActive\n
+1 - Change Rank for Survivor\n
+2 - Change Rank for Killer\n
+3 - Exit\n
+== == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == \n
+Enter a choice and press enter: ''')
---
+++ response.py
@@ -0,0 +1,134 @@
+import re
+import requests
+import urllib3
+import psutil
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+if "Fiddler.exe" in (p.name() for p in psutil.process_iter()):
+    proxies = {
+        "http": "http://127.0.0.1:8888",
+        "https": "http://127.0.0.1:8888", }
+if not "Fiddler.exe" in (p.name() for p in psutil.process_iter()):
+    proxies = {"http": "", "https": "", }
+
+
+def checkCookie(cookie):
+    r = requests.get('https://steam.live.bhvrdbd.com/api/v1/wallet/currencies',
+                     cookies={'bhvrSession': cookie},
+                     headers={'Host': 'steam.live.bhvrdbd.com',
+                              'User-Agent': 'DeadByDaylight/++DeadByDaylight+Live-CL-321933 Windows/10.0.19041.1.768.64bit',
+                              'Content-Type': 'application/json; charset=utf-8',
+                              'x-kraken-client-platform': 'steam'},proxies=proxies,
+                     verify=False)
+    if r.status_code == 200:
+        return True
+    else:
+        return False
+
+
+def rankUpSurv(cookie):
+    rank = requests.put('https://steam.live.bhvrdbd.com/api/v1/ranks/pips',
+                        cookies={'bhvrSession': cookie},
+                        headers={'Content-Type': 'application/json'},
+                        json={"forceReset": False, "killerPips": 0, "survivorPips": 1}, proxies=proxies, verify=False)
+    print("Ranking Up...")
+    if rank.status_code == 200:
+        print("True")
+
+
+def rankDownSurv(cookie):
+    rank = requests.put('https://steam.live.bhvrdbd.com/api/v1/ranks/pips',
+                        cookies={'bhvrSession': cookie},
+                        headers={'Content-Type': 'application/json'},
+                        json={"forceReset": False, "killerPips": 0, "survivorPips": -1}, proxies=proxies, verify=False)
+    print("Ranking Down...")
+    if rank.status_code == 200:
+        print("True")
+
+
+def rankUpKiller(cookie):
+    rank = requests.put('https://steam.live.bhvrdbd.com/api/v1/ranks/pips',
+                        cookies={'bhvrSession': cookie},
+                        headers={'Content-Type': 'application/json'},
+                        json={"forceReset": False, "killerPips": 1, "survivorPips": 0}, proxies=proxies, verify=False)
+    print("Ranking Up...")
+    if rank.status_code == 200:
+        print("True")
+
+
+def rankDownKiller(cookie):
+    rank = requests.put('https://steam.live.bhvrdbd.com/api/v1/ranks/pips',
+                        cookies={'bhvrSession': cookie},
+                        headers={'Content-Type': 'application/json'},
+                        json={"forceReset": False, "killerPips": -1, "survivorPips": 0}, proxies=proxies, verify=False)
+    print("Ranking Down....")
+    if rank.status_code == 200:
+        print("True")
+
+
+def rankGetSurv(cookie):
+    response = requests.get('https://steam.live.bhvrdbd.com/api/v1/ranks/pips',
+                            cookies={'bhvrSession': cookie}, proxies=proxies, verify=False)
+
+    message = response.text
+    re.findall(r"\d+", message)
+    num = [int(nu) for nu in re.findall(r"\d+", message)]
+
+    return findRank(num[0])
+
+
+def rankGetKiller(cookie):
+    response = requests.get('https://steam.live.bhvrdbd.com/api/v1/ranks/pips',
+                            cookies={'bhvrSession': cookie}, proxies=proxies, verify=False)
+    message = response.text
+    re.findall(r"\d+", message)
+    num = [int(nu) for nu in re.findall(r"\d+", message)]
+
+    return findRank(num[2])
+
+
+def findRank(numofpips):
+    rank = -1
+    if numofpips == 0 or numofpips == 1 or numofpips == 2:
+        rank = 20
+    if numofpips == 3 or numofpips == 4 or numofpips == 5:
+        rank = 19
+    if numofpips == 6 or numofpips == 7 or numofpips == 8 or numofpips == 9:
+        rank = 18
+    if numofpips == 10 or numofpips == 11 or numofpips == 12 or numofpips == 13:
+        rank = 17
+    if numofpips == 14 or numofpips == 15 or numofpips == 16 or numofpips == 17:
+        rank = 16
+    if numofpips == 18 or numofpips == 19 or numofpips == 20 or numofpips == 21:
+        rank = 15
+    if numofpips == 22 or numofpips == 23 or numofpips == 24 or numofpips == 25:
+        rank = 14
+    if numofpips == 26 or numofpips == 27 or numofpips == 28 or numofpips == 29:
+        rank = 13
+    if numofpips == 30 or numofpips == 31 or numofpips == 32 or numofpips == 33 or numofpips == 34:
+        rank = 12
+    if numofpips == 35 or numofpips == 36 or numofpips == 37 or numofpips == 38 or numofpips == 39:
+        rank = 11
+    if numofpips == 40 or numofpips == 41 or numofpips == 42 or numofpips == 43 or numofpips == 44:
+        rank = 10
+    if numofpips == 45 or numofpips == 46 or numofpips == 47 or numofpips == 48 or numofpips == 49:
+        rank = 9
+    if numofpips == 50 or numofpips == 51 or numofpips == 52 or numofpips == 53 or numofpips == 54:
+        rank = 8
+    if numofpips == 55 or numofpips == 56 or numofpips == 57 or numofpips == 58 or numofpips == 59:
+        rank = 7
+    if numofpips == 60 or numofpips == 61 or numofpips == 62 or numofpips == 63 or numofpips == 64:
+        rank = 6
+    if numofpips == 65 or numofpips == 66 or numofpips == 67 or numofpips == 68 or numofpips == 69:
+        rank = 5
+    if numofpips == 70 or numofpips == 71 or numofpips == 72 or numofpips == 73 or numofpips == 74:
+        rank = 4
+    if numofpips == 75 or numofpips == 76 or numofpips == 77 or numofpips == 78 or numofpips == 79:
+        rank = 3
+    if numofpips == 80 or numofpips == 81 or numofpips == 82 or numofpips == 83 or numofpips == 84:
+        rank = 2
+    if numofpips == 85:
+        rank = 1
+
+    return rank
